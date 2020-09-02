cc040d45028573d72939f72a2b1aef25a596c370
vcarlsberg <v.carlsberg@gmail.com>
2020-09-02 08:47:37 +0000

    Update modelling script - this to remove data with same description

--- modelling.R
+++ modelling.R
@@ -1,4 +1,10 @@
 library(car)
+library(dplyr)
 
-linear.model<-lm(price~0+bedroom+bathroom+carport+buildingarea+landarea,data = house123)
+modelling_data<-house123 %>% distinct(description, .keep_all= TRUE)
+
+
+linear.model<-lm(price~0+bedroom+bathroom+carport+buildingarea+landarea,data = modelling_data)
 summary(linear.model)
+
+#vif(linear.model)
