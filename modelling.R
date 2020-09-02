211d4c5d32b1a7b012a3144cc47e9d78ca0e849a
vcarlsberg <v.carlsberg@gmail.com>
2020-09-02 10:37:55 +0000

    save rdata and model

--- modelling.R
+++ modelling.R
@@ -4,7 +4,7 @@ library(dplyr)
 modelling_data<-house123 %>% distinct(description, .keep_all= TRUE)
 
 
-linear.model<-lm(price~0+bedroom+bathroom+carport+buildingarea+landarea,data = modelling_data)
+linear.model<-lm(price~bedroom+bathroom+carport+buildingarea+landarea,data = modelling_data)
 summary(linear.model)
 
 vif(linear.model)
